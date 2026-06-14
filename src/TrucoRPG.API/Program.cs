a071f7fd8ea7ab96f4e760d4ce45dc1a7e875492
finkguido <guidofink01@hotmail.com>
2026-06-14 19:08:36 -0300

    actualizo Program.cs migraciones

--- src/TrucoRPG.API/Program.cs
+++ src/TrucoRPG.API/Program.cs
@@ -129,6 +129,12 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await db.Database.MigrateAsync();
+    }
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
